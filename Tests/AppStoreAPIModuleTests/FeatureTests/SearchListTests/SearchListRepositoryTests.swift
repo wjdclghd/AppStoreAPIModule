634c409218c8da864cd1bc99ac6d2ac10d69fb7f
jch <jeongchihong@sk.com>
2025-07-07 14:47:49 +0900

    [feature-searchList] unittest 추가 - Feature Module 분리로 코드 수정 및 검토 필요

--- Tests/AppStoreAPIModuleTests/FeatureTests/SearchListTests/SearchListRepositoryTests.swift
+++ Tests/AppStoreAPIModuleTests/FeatureTests/SearchListTests/SearchListRepositoryTests.swift
@@ -6,3 +6,143 @@
 //
 
 import Foundation
+import XCTest
+import Combine
+@testable import iOSCleanArchitecture
+@testable import CoreDatabase
+
+final class SearchListRepositoryTests: XCTestCase {
+    private var testCancellables: Set<AnyCancellable>!
+    private var testSearchListDatabase: TestSearchListDatabase!
+    private var testSearchListRepository: SearchListRepository!
+
+    override func setUp() {
+        testCancellables = []
+        let testSearchListDatabase = TestSearchListDatabase()
+        testSearchListRepository = SearchListRepository(searchListDatabaseProtocol: testSearchListDatabase)
+    }
+
+    override func tearDown() {
+        testCancellables = nil
+        testSearchListDatabase = nil
+        testSearchListRepository = nil
+    }
+
+    func testSelectDatabase() {
+        let testExpectation = XCTestExpectation(description: "TestSelectDatabase")
+
+        testSearchListRepository.selectRepositoryProtocol(searchKeyword: "TestKeyword")
+            .sink(
+                receiveCompletion: { _ in
+                },
+                receiveValue: { result in
+                    XCTAssertEqual(result.count, 1)
+                    XCTAssertEqual(result.first?.searchKeyword, "TestKeyword")
+                    
+                    testExpectation.fulfill()
+                }
+            )
+            .store(in: &testCancellables)
+
+        wait(for: [testExpectation], timeout: 1)
+    }
+
+    func testSelectAllDatabase() {
+        let testExpectation = XCTestExpectation(description: "TestSelectAllDatabase")
+
+        testSearchListRepository.selectAllRepositoryProtocol()
+            .sink(
+                receiveCompletion: { _ in
+                },
+                receiveValue: { result in
+                    XCTAssertEqual(result.count, 2)
+                    
+                    testExpectation.fulfill()
+                }
+            )
+            .store(in: &testCancellables)
+
+        wait(for: [testExpectation], timeout: 1)
+    }
+}
+
+final class TestSearchListDatabase: SearchListDatabaseProtocol {
+    var lastInsertedEntity: SearchListEntity?
+    var lastDeletedKeyword: String?
+    var insertCalled = false
+    var shouldFail = false
+    
+    func insertDatabase<T: SearchListEntityProtocol>(searchListEntity: T) -> AnyPublisher<Void, Error> {
+        insertCalled = true
+        
+        if let testEntity = searchListEntity as? SearchListEntity {
+            lastInsertedEntity = testEntity
+        }
+
+        return shouldFail
+            ? Fail(error: NSError(domain: "", code: -1))
+            .eraseToAnyPublisher()
+            : Just(())
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+    
+    func updateDatabase<T: SearchListEntityProtocol>(searchListEntity: T) -> AnyPublisher<[T], Error> {
+        return shouldFail
+            ? Fail(error: NSError(domain: "", code: -1))
+            .eraseToAnyPublisher()
+            : Just([searchListEntity])
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+
+    func deleteDatabase(searchKeyword: String) -> AnyPublisher<Void, Error> {
+        lastDeletedKeyword = searchKeyword
+        return shouldFail
+            ? Fail(error: NSError(domain: "", code: -1))
+            .eraseToAnyPublisher()
+            : Just(())
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+
+    func deleteAllDatabase() -> AnyPublisher<Void, Error> {
+        return shouldFail
+            ? Fail(error: NSError(domain: "", code: -1))
+            .eraseToAnyPublisher()
+            : Just(())
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+
+    func selectDatabase<T: SearchListEntityProtocol>(searchKeyword: String) -> AnyPublisher<[T], Error> {
+        guard let testSearchKeyword = SearchListEntity(searchKeyword: searchKeyword) as? T else {
+            return Fail(error: NSError(domain: "CastingError", code: -2))
+                .eraseToAnyPublisher()
+        }
+
+        return shouldFail
+            ? Fail(error: NSError(domain: "", code: -1))
+            .eraseToAnyPublisher()
+            : Just([testSearchKeyword]).setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+
+    func selectAllDatabase<T: SearchListEntityProtocol>() -> AnyPublisher<[T], Error> {
+        let testEntitys = [
+            SearchListEntity(searchKeyword: "selectAll1"),
+            SearchListEntity(searchKeyword: "selectAll2")
+        ]
+        
+        guard let testList = testEntitys as? [T] else {
+            return Fail(error: NSError(domain: "CastingError", code: -2))
+                .eraseToAnyPublisher()
+        }
+
+        return shouldFail
+            ? Fail(error: NSError(domain: "", code: -1))
+            .eraseToAnyPublisher()
+            : Just(testList).setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+}
--- Tests/AppStoreAPIModuleTests/FeatureTests/SearchListTests/SearchListViewModelTests.swift
+++ Tests/AppStoreAPIModuleTests/FeatureTests/SearchListTests/SearchListViewModelTests.swift
@@ -6,3 +6,241 @@
 //
 
 import Foundation
+import XCTest
+import Combine
+import SwiftUI
+@testable import iOSCleanArchitecture
+
+final class SearchListViewModelTests: XCTestCase {
+    private var testCancellables: Set<AnyCancellable>!
+    private var testSearchListUseCase: TestSearchListUseCase!
+    private var testSearchListViewModel: SearchListViewModel!
+
+    override func setUpWithError() throws {
+        testCancellables = []
+        testSearchListUseCase = TestSearchListUseCase()
+        testSearchListUseCase.testSkipSelectAll = true
+        testSearchListViewModel = SearchListViewModel(useCase: testSearchListUseCase)
+    }
+
+    override func tearDownWithError() throws {
+        testCancellables = nil
+        testSearchListUseCase = nil
+        testSearchListViewModel = nil
+    }
+    
+    func testSearchKeywordBind() {
+        let testExpectation = expectation(description: "TestSearchKeywordBind")
+        
+        testSearchListUseCase.testUseCaseEntitys = [
+                SearchListEntity(searchKeyword: "TestSearchKeyword")
+        ]
+        
+        testSearchListViewModel.$searchEntitys
+            .drop(while: { $0.isEmpty })
+            .sink { entitys in
+                XCTAssertEqual(entitys.count, 1)
+                XCTAssertEqual(entitys.first?.searchKeyword, "TestSearchKeyword")
+                testExpectation.fulfill()
+            }
+            .store(in: &testCancellables)
+
+        testSearchListViewModel.searchKeyword = "TestSearchKeyword"
+
+        wait(for: [testExpectation], timeout: 1.5)
+    }
+    
+    func testSearchKeywordSave() {
+        let testExpectation = expectation(description: "TestSearchKeywordSave")
+        let testSearchKeyword = "TestSearchKeyword"
+
+        testSearchListUseCase.testInsertCalled = false
+        testSearchListUseCase.testInserteSearchKeyword = nil
+        testSearchListUseCase.testInsertExpectation = testExpectation
+
+        testSearchListViewModel.searchKeywordSave(searchKeyword: testSearchKeyword)
+
+        wait(for: [testExpectation], timeout: 1.5)
+
+        XCTAssertTrue(testSearchListUseCase.testInsertCalled)
+        XCTAssertEqual(testSearchListUseCase.testInserteSearchKeyword, testSearchKeyword)
+    }
+    
+    func testInitSearchKeywordAllList() {
+        let testExpectation = expectation(description: "TestInitSearchKeywordAllList")
+        
+        testSearchListUseCase.testSkipSelectAll = false
+        
+        testSearchListUseCase.testUseCaseEntitys = [
+                SearchListEntity(searchKeyword: "TestSearchKeyword1"),
+                SearchListEntity(searchKeyword: "TestSearchKeyword2")
+        ]
+        
+        testSearchListViewModel = SearchListViewModel(useCase: testSearchListUseCase)
+
+        testSearchListViewModel.$searchEntitys
+            .dropFirst()
+            .sink { entitys in
+                XCTAssertEqual(entitys.count, 2)
+                XCTAssertEqual(entitys[0].searchKeyword, "TestSearchKeyword1")
+                XCTAssertEqual(entitys[1].searchKeyword, "TestSearchKeyword2")
+                testExpectation.fulfill()
+            }
+            .store(in: &testCancellables)
+
+        wait(for: [testExpectation], timeout: 1.5)
+    }
+    
+    func testInitSearchKeywordAllListFail() {
+        let testExpectation = expectation(description: "TestInitSearchKeywordAllListFail")
+        
+        testSearchListUseCase.testFailSelectAll = true
+        testSearchListViewModel = SearchListViewModel(useCase: testSearchListUseCase)
+
+        testSearchListViewModel.$errorMessage
+            .drop(while: { $0 == nil })
+            .sink { error in
+                XCTAssertEqual(error, "SelectAll Error")
+                testExpectation.fulfill()
+            }
+            .store(in: &testCancellables)
+
+        wait(for: [testExpectation], timeout: 1.5)
+    }
+    
+    func testSearchKeywordBindFail() {
+        let testExpectation = expectation(description: "TestSearchKeywordBindFail")
+        testSearchListUseCase.testFailBind = true
+
+        testSearchListViewModel.$errorMessage
+            .drop(while: { $0 == nil })
+            .sink { error in
+                XCTAssertEqual(error, "Bind Error")
+                testExpectation.fulfill()
+            }
+            .store(in: &testCancellables)
+
+        testSearchListViewModel.searchKeyword = "TestSearchKeyword"
+
+        wait(for: [testExpectation], timeout: 1.5)
+    }
+    
+    func testSearchKeywordInsertFail() {
+        let testExpectation = expectation(description: "TestSearchKeywordInsertFail")
+        testSearchListUseCase.testFailInsert = true
+
+        testSearchListViewModel.$errorMessage
+            .drop(while: { $0 == nil })
+            .sink { error in
+                XCTAssertNotNil(error)
+                XCTAssertEqual(error, "Insert Error")
+                testExpectation.fulfill()
+            }
+            .store(in: &testCancellables)
+        
+        testSearchListViewModel.searchKeywordSave(searchKeyword: "TestInsertFailSearchKeyword")
+
+        wait(for: [testExpectation], timeout: 1.5)
+    }
+    
+    func testSearchKeywordSelectFail() {
+        let testExpectation = expectation(description: "TestSearchKeywordSelectFail")
+        testSearchListUseCase.testFailSelect = true
+
+        testSearchListViewModel.$errorMessage
+            .drop(while: { $0 == nil })
+            .sink { error in
+                XCTAssertEqual(error, "Select Error")
+                testExpectation.fulfill()
+            }
+            .store(in: &testCancellables)
+
+        testSearchListViewModel.searchKeywordList(searchKeyword: "TestSearchKeyword")
+
+        wait(for: [testExpectation], timeout: 1.5)
+    }
+}
+
+final class TestSearchListUseCase: SearchListUseCaseProtocol {
+    var testSkipSelectAll = false
+    var testFailSelectAll = false
+    var testFailInsert = false
+    var testFailSelect = false
+    var testFailBind = false
+    var testInsertCalled = false
+    var testInserteSearchKeyword: String?
+    var testInsertExpectation: XCTestExpectation?
+    var testUseCaseEntitys: [SearchListEntity] = []
+    
+    func insertUseCaseProtocol(searchKeyword: String) -> AnyPublisher<Void, Error> {
+        testInsertCalled = true
+        testInserteSearchKeyword = searchKeyword
+        testInsertExpectation?.fulfill()
+        
+        if testFailInsert {
+            return Fail(error: NSError(domain: "Test", code: -1, userInfo: [NSLocalizedDescriptionKey: "Insert Error"]))
+                .eraseToAnyPublisher()
+        }
+        
+        return Just(())
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+
+    func updateUseCaseProtocol(searchKeyword: String) -> AnyPublisher<[SearchListEntity], Error> {
+        return Just([SearchListEntity(searchKeyword: searchKeyword)])
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+
+    func deleteUseCaseProtocol(searchKeyword: String) -> AnyPublisher<Void, Error> {
+        return Just(())
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+
+    func deleteAllUseCaseProtocol() -> AnyPublisher<Void, Error> {
+        return Just(())
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+
+    func selectUseCaseProtocol(searchKeyword: String) -> AnyPublisher<[SearchListEntity], Error> {
+        if testFailSelect {
+            return Fail(error: NSError(domain: "Test", code: -1, userInfo: [NSLocalizedDescriptionKey: "Select Error"]))
+                .eraseToAnyPublisher()
+        }
+        
+        return Just(testUseCaseEntitys)
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+
+    func selectAllUseCaseProtocol() -> AnyPublisher<[SearchListEntity], Error> {
+        if testFailSelectAll {
+            return Fail(error: NSError(domain: "Test", code: -1, userInfo: [NSLocalizedDescriptionKey: "SelectAll Error"]))
+                .eraseToAnyPublisher()
+        }
+        
+        if testSkipSelectAll {
+            return Just([])
+                .setFailureType(to: Error.self)
+                .eraseToAnyPublisher()
+        }
+        
+        return Just(testUseCaseEntitys)
+        .setFailureType(to: Error.self)
+        .eraseToAnyPublisher()
+    }
+    
+    func selectSearchKeywordBind(searchKeyword: String) -> AnyPublisher<[iOSCleanArchitecture.SearchListEntity], any Error> {
+        if testFailBind {
+            return Fail(error: NSError(domain: "Test", code: -1, userInfo: [NSLocalizedDescriptionKey: "Bind Error"]))
+                .eraseToAnyPublisher()
+        }
+        
+        return Just(testUseCaseEntitys)
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+}
